b0a5cd4e06ab9b1e7471346c547f6e4d07c5079d
Manuel Haddadi <manuel.haddadi@gfi.fr>
2017-07-26 11:38:27 +0200

    Adjustment to GFI's LDAP

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # LDAP users to Gitlab
 
-This script gets all users from LDAP. For each user :
- * if his group does not exist in Gitlab, it creates it ;
- * if the user does not exist in Gitlab, it creates it ;
- * if the user is not a member of his group in Gitlab, it adds it.
+This script synchronizes Gitlab users and groups with LDAP :
+ * it creates LDAP users into Gitlab ;
+ * it creates LDAP groups into Gitlab ;
+ * it adds LDAP group members into Gitlab.
 
  ### Prerequisites
  * use of ldapsearch from ldap-utils package
--- copy_users_groups_from_ldap_to_gitlab.sh
+++ copy_users_groups_from_ldap_to_gitlab.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #
-# This script gets all users from LDAP. For each user :
-# - if his group does not exist in Gitlab, it creates it ;
-# - if the user does not exist in Gitlab, it creates it ;
-# - if the user is not a member of his group in Gitlab, it adds it.
+# This script :
+# - creates LDAP users into Gitlab ;
+# - creates LDAP groups into Gitlab ;
+# - adds LDAP group members into Gitlab.
 #
 # Prerequisites : use of ldapsearch from ldap-utils package
 
 LDAP_URL="ldap://0.0.0.0"
-LDAP_USERS_DN="ou=people,dc=ldap,dc=test,dc=org objectClass=person"
+LDAP_USERS_REQUEST="dc=gfi,dc=fr objectClass=inetOrgPerson"
+LDAP_GROUPS_REQUEST="dc=gfi,dc=fr objectClass=groupOfUniqueNames"
 
 GITLAB_API_URL="http://127.0.0.1/api/v4"
-GITLAB_ADMIN_PRIVATE_TOKEN="8jH4Kzs_N1mTjsePRLzR"
+GITLAB_ADMIN_PRIVATE_TOKEN="fQH7coWrbeuJGMKYkPgz"
 
 _gitlabGroupId=""
-_gitlabUserId=""
 
-# Search for gitlab group. If it does not exist, it creates it.
+# Searches for gitlab group. If it does not exist, it creates it.
 processGroup() {
   local group=$1
 
@@ -24,14 +24,16 @@ processGroup() {
 
   if [[ "[]" == "$response" ]]; then
     # Creating Gitlab group
-    echo "Creating Gitlab group : $group"
+    echo "-> Creating Gitlab group : $group"
     response=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_ADMIN_PRIVATE_TOKEN" --data "name=$group&path=$group" -XPOST "$GITLAB_API_URL/groups")
+    echo $reponse
   fi
 
+  # Set current Gitlab group id
   _gitlabGroupId=$(echo $response | sed 's/\(.*\"id\":\)\([0-9]*\)\(,.*\)/\2/')
 }
 
-# Search for gitlab user. If it does not exist, it creates it.
+# Searches for gitlab user. If it does not exist, it creates it.
 processUser() {
   local uid=$1
   local name=$2
@@ -41,65 +43,81 @@ processUser() {
 
   if [[ "[]" == "$response" ]]; then
     # Creating Gitlab user
-    echo "Creating Gitlab user username : $uid, e-mail : $mail, name : $name"
-    response=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_ADMIN_PRIVATE_TOKEN" --data "username=$uid&email=$mail&name=$name&reset_password=true" -XPOST "$GITLAB_API_URL/users")
+    echo "-> Creating Gitlab user username : $uid, e-mail : $mail, name : $name"
+    echo $(curl -s --header "PRIVATE-TOKEN: $GITLAB_ADMIN_PRIVATE_TOKEN" --data "username=$uid&email=$mail&name=$name&reset_password=true" -XPOST "$GITLAB_API_URL/users")
   fi
 
-  # Return Gitlab user id
-  _gitlabUserId=$(echo $response | sed 's/\(.*\"id\":\)\([0-9]*\)\(,.*\)/\2/')
 }
 
-# Main part : basically, for each LDAP user...
+# Sync LDAP users into Gitlab
+# ---------------------------
 REGEX_UID="uid: (.*)"
 REGEX_NAME="cn: (.*)"
 REGEX_MAIL="mail: (.*)"
-REGEX_GROUPE="ou: (.*)"
 
-uid=''
-name=''
-mail=''
-group=''
+uid=""
+name=""
+mail=""
 
-ldapsearch -xLLL -H $LDAP_URL -b $LDAP_USERS_DN uid cn mail ou |
+ldapsearch -xLLL -H $LDAP_URL -b $LDAP_USERS_REQUEST uid cn mail |
 while IFS= read -r line;
 do
 
-  if [[ $line =~ $REGEX_UID ]]; then
+  if [[ "$line" =~ $REGEX_UID ]]; then
     uid="$(echo "$line" | sed 's/uid: \(.*\)/\1/')"
   fi
 
-  if [[ $line =~ $REGEX_NAME ]]; then
+  if [[ "$line" =~ $REGEX_NAME ]]; then
     name="$(echo "$line" | sed 's/cn: \(.*\)/\1/')"
   fi
 
-  if [[ $line =~ $REGEX_MAIL ]]; then
+  if [[ "$line" =~ $REGEX_MAIL ]]; then
     mail="$(echo "$line" | sed 's/mail: \(.*\)/\1/')"
   fi
 
-  if [[ $line =~ $REGEX_GROUPE ]]; then
-    group=$(echo "$line" | sed 's/ou: \(.*\)/\1/')
+  if [[ "$uid" != "" && "$name" != "" && "$mail" != "" ]]; then
+    processUser "$uid" "$name" "$mail"
+    uid=""
+    name=""
+    mail=""
   fi
 
-  if [[ "$uid" != "" && "$name" != "" && "$mail" != "" && "$group" != "" ]]; then
+done
 
-    processGroup "$group"
+# Sync LDAP groups and members into Gitlab
+# ----------------------------------------
+REGEX_GROUP_NAME="dn: cn=(.*)"
+REGEX_MEMBER="uniqueMember: uid=(.*)"
 
-    processUser "$uid" "$name" "$mail"
+groupName=""
 
-    if [[ "$_gitlabUserId" != "" && "$_gitlabGroupId" != "" ]]; then
-      echo "Adding user $uid (Gitlab user id $_gitlabUserId) as a member to group $group (Gitlab group id $_gitlabGroupId) :"
+ldapsearch -xLLL -H $LDAP_URL -b $LDAP_GROUPS_REQUEST uniqueMember |
+while IFS= read -r line;
+do
 
-      curl -s --header "PRIVATE-TOKEN: $GITLAB_ADMIN_PRIVATE_TOKEN" -XPOST "$GITLAB_API_URL/groups/$_gitlabGroupId/members?user_id=$_gitlabUserId&access_level=30"
+  if [[ "$line" =~ $REGEX_GROUP_NAME ]]; then
+    _gitlabGroupId=""
+    groupName="$(echo "$line" | sed 's/dn: cn=\(.[^,]*\)\(.*\)/\1/')"
+    processGroup "$groupName"
+  fi
 
-      echo ""
+  if [[ "$line" =~ $REGEX_MEMBER ]]; then
+
+    memberUid="$(echo "$line" | sed 's/uniqueMember: uid=\(.[^,]*\)\(.*\)/\1/')"
+
+    if [[ "$memberUid" != "none" ]]; then
+
+      gitlabUserId=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_ADMIN_PRIVATE_TOKEN" -XGET "$GITLAB_API_URL/users?username=$memberUid" | sed 's/\(.*\"id\":\)\([0-9]*\)\(,.*\)/\2/')
+
+      if [[ "$gitlabUserId" == "[]" ]]; then
+        echo "-> ERROR : user $memberUid was not created previously in Gitlab"
+      else
+        if [[ "$gitlabUserId" != "" && "$_gitlabGroupId" != "" ]]; then
+          echo "-> Adding user $memberUid (Gitlab user id $gitlabUserId) as a member to group $groupName (Gitlab group id $_gitlabGroupId) :"
+          echo $(curl -s --header "PRIVATE-TOKEN: $GITLAB_ADMIN_PRIVATE_TOKEN" -XPOST "$GITLAB_API_URL/groups/$_gitlabGroupId/members?user_id=$gitlabUserId&access_level=30")
+        fi
+      fi
     fi
-
-    uid=''
-    name=''
-    mail=''
-    group=''
-    _gitlabGroupId=''
-    _gitlabUserId=''
   fi
 
 done
